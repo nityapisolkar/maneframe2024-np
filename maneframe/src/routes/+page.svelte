5e40a33fa1dd529fe0df03058fdd94d71cf31871
Rohan Bansal <bansalrohan18@gmail.com>
2024-05-24 03:11:48 -0400

    added resopnsivity + faq+ better sched

--- maneframe/src/routes/+page.svelte
+++ maneframe/src/routes/+page.svelte
@@ -13,11 +13,11 @@
 
 
 <body class="overflow-x-hidden w-screen h-screen bg-gradient-to-b from-black to-indigo-900 resize-none">
-		<div class="w-screen mt-16 mb-20">
+		<div class="w-screen mt-16 mb-4">
 			<img src="/img.png" alt="logo" class="m-auto object-scale-down" width="300"/>
 		</div>
-		<div class="w-screen h-2/5 text-center items-center content-center justify-center pb-32 mb-20">
-			<h1 class="text-5xl sm:text-9xl font-bold animate-bounce-small sm:text-center text-white pt-12 mb-10 sm:m-20 sm:mt-32">ManeFrame</h1>
+		<div class="w-screen text-center items-center content-center justify-center mb-1">
+			<h1 class="text-7xl lg:text-9xl font-bold animate-bounce-small sm:text-center text-white pt-12 mb-10 sm:m-20 sm:mt-16 ">ManeFrame</h1>
 			<Typewriter mode="loop" wordInterval="3000" class="">
 				<p class="text-2xl sm:text-5xl font-italic, text-center, animate-pulse text-white m-10" id="llvm">A Hackathon for Middle and High School Students</p>
 				<p class="text-2xl sm:text-5xl font-italic, text-center, animate-pulse text-white m-10">June 15, 2024 @ MLK Library, San Jose State University</p>
@@ -25,40 +25,21 @@
 		</div>
 		<!-- <div class="divider text-white divider-accent sm:text-4xl">
 		</div> -->
+
+		<div class="w-screen flex text-center content-center justify-center content-center ">
+			<div class="text-center w-3/4 sm:w-1/2 sm:m-12">
+				<a href="/apply" id="llvmrefernce"><button class="button bg-white text-black text-xl md:text-2xl p-4 2xl:p-10 md:m-2 rounded-xl">APPLY</button></a>
+
+			</div>
+		</div>
 		<div class="w-screen h-1/2 flex text-center content-center justify-center">
 
 			<div class="text-center w-3/4 sm:w-1/2 sm:m-12">
-				<h1 class="text-white text-2xl lg:text-3xl xl:text-5xl xl:mt-16 lg:ml-16 lg:mr-16 lg:mt-2 mt-8 font-bold mb-8 hover:animate-pulse">Hey!</h1>
+				<h1 class="text-white text-3xl lg:text-3xl xl:text-5xl xl:mt-16 lg:ml-16 lg:mr-16 lg:mt-2 mt-8 font-bold mb-8 p-4 hover:animate-pulse">Hey!</h1>
 				<p class="text-white text-xl lg:text-2xl 2xl:text3xl text-center justify-center content-center mb-4">Do you want to make something cool? Spend some time with friends? Or maybe learn something new? If so, you should come to the ManeFrame hackathon! Whether you're a seasoned programmer or just starting out, we have a place for you!</p>
 			</div>
 		</div>
-		<div class="divider text-white divider-accent sm:text-4xl">
-		</div>
-		<div class="w-screen h-1/2 flex text-center content-center justify-center">
-
-			<div class="text-center w-3/4 sm:w-1/2 sm:m-12">
-				<h1 class="text-white text-2xl lg:text-3xl xl:text-5xl xl:mt-16 lg:ml-16 lg:mr-16 lg:mt-2 mt-8 font-bold mb-8 hover:animate-pulse">Guest Speakers</h1>
-				<div class="grid grid-cols-2 flex">
-					<div class="justify-center content-center">
-						<h1 class="text-2xl mb-10 underline decoration-dashed">Viraj Kadakia</h1>
-						<img src="viraj.jpeg" alt="Viraj Kadakia" width="200" class="m-auto"/>
-					</div>
-					<div class="justify-center content-center">
-						<h1 class="text-2xl mb-10 underline decoration-dashed">Sagar Joshi</h1>
-						<img src="sagar.jpeg" alt="Sagar Joshi" width="200" class="m-auto"/>
-					</div>
-				</div>
-			</div>
-		</div>
 		<div class="divider text-white divider-accent sm:text-4xl sm:mt-8">
-		</div>
-		<div class="w-screen h-1/2 flex text-center content-center justify-center content-center">
-			<div class="text-center w-3/4 sm:w-1/2 sm:m-12">
-				<h1 class="text-white text-2xl lg:text-3xl xl:text-5xl xl:mt-16 lg:ml-16 lg:mr-16 lg:mt-2 mt-8 font-bold mb-8 hover:animate-pulse">Join Us!</h1>
-				<p class="text-white text-md sm:text-xl text-center justify-center content-center mb-4">ManeFrame will be at the Dr. Martin Luther King Jr. Library in San Jose on June 15, 2024.</p>
-				<a href="/apply" id="llvmrefernce"><button class="button bg-white text-black text-xl md:text-2xl p-4 md:p-2 2xl:p-10 md:m-2 rounded-xl">APPLY</button></a>
-
-			</div>
 		</div>
 		<!---->
 		<div class="divider text-white divider-accent sm:text-4xl sm:mt-8">
@@ -68,29 +49,110 @@
 				<h1 class="text-white text-2xl lg:text-3xl xl:text-5xl xl:mt-16 lg:ml-16 lg:mr-16 lg:mt-2 mt-8 font-bold hover:animate-pulse">Frequently Asked Questions</h1>
 			</div>
 		</div>
-		<div class="w-screen h-1/2 sm:flex text-center content-center justify-center">
-			<div class="text-center sm:w-1/4 m-10">
-				<h1 class="text-white text-xl lg:text-2xl xl:text-5xl xl:mt-16 lg:ml-16 lg:mr-16 lg:mt-2  mt-8 font-bold mb-8 hover:animate-pulse">What is a Hackathon?</h1>
+		<div class="w-screen sm:flex text-center content-center justify-center">
+			<div class="text-center sm:w-1/4 m-10 p-4 border-white rounded-md border-2">
+				<h1 class="text-white sm:text-2xl xl:text-4xl lg:ml-16 lg:mr-16 font-bold mb-8 mt-8 hover:animate-pulse">What is a Hackathon?</h1>
 				<p class="text-white text-md lg:text-xl text-center justify-center content-center mb-4">A Hackathon is an event where you code a project for a certain amount of time and then get it judged. It is meant to be a fun event where you can create anything you want with freedom, and where you can explore the amazing world of computer science.</p>
 			</div>
-			<div class="text-center sm:w-1/4 m-10">
-				<h1 class="text-white text-xl lg:text-2xl xl:text-5xl xl:mt-16 lg:ml-16 lg:mr-16 lg:mt-2  mt-8 font-bold mb-8 hover:animate-pulse">What do I make?</h1>
+			<div class="text-center sm:w-1/4 m-10 p-4 border-white rounded-md border-2">
+				<h1 class="text-white sm:text-2xl xl:text-4xl lg:ml-16 lg:mr-16 font-bold mb-8 mt-8 hover:animate-pulse">What do I make?</h1>
 				<p class="text-white text-md lg:text-xl text-center justify-center content-center mb-4">You can make whatever you want! From a simple calculator app to an online chat portal, feel free to make or submit anything. We encourage exploring and trying out new things!</p>
 			</div>
-			<div class="text-center sm:w-1/4 m-10">
-				<h1 class="text-white text-xl lg:text-2xl xl:text-5xl xl:mt-16 lg:ml-16 lg:mr-16 lg:mt-2 mt-8 font-bold mb-8 hover:animate-pulse">Does it cost money?</h1>
+			<div class="text-center sm:w-1/4 m-10 p-4 border-white rounded-md border-2">
+				<h1 class="text-white sm:text-2xl xl:text-4xl lg:ml-16 lg:mr-16 font-bold mb-8 mt-8 hover:animate-pulse">Does it cost money?</h1>
 				<p class="text-white text-md lg:text-xl text-center justify-center content-center mb-4">Nope! Maneframe is absolutely free! We encourage anyone in middle or high school to attend.</p>
 			</div>
 		</div>
-		<div class="divider text-white divider-accent sm:text-4xl">
+		<div class="divider text-white divider-accent sm:text-4xl sm:mt-8">
 		</div>
+		<!---->
+		<div class="divider text-white divider-accent sm:text-4xl sm:mt-8">
+		</div>
+		<div class="w-screen flex text-center content-center justify-center">
+			<div class="text-center w-3/4 sm:w-1/2 sm:m-8">
+				<h1 class="text-white text-2xl lg:text-3xl xl:text-5xl xl:mt-16 lg:ml-16 lg:mr-16 lg:mt-2 mt-8 font-bold hover:animate-pulse">Guest Speakers</h1>
+			</div>
+		</div>
+		<div class="w-screen sm:flex text-center content-center justify-center">
+			<div class="text-center sm:w-1/4 m-10 p-4 border-white rounded-md border-2">
+				<h1 class="text-2xl mb-10 underline text-white decoration-dashed">Viraj Kadakia</h1>
+				<img src="viraj.jpeg" alt="Viraj Kadakia" width="200" class="m-auto"/>
+			</div>
+			<div class="text-center sm:w-1/4 m-10 p-4 border-white rounded-md border-2">
+				<h1 class="text-2xl mb-10 underline text-white decoration-dashed">Sagar Joshi</h1>
+				<img src="sagar.jpeg" alt="Sagar Joshi" width="200" class="m-auto"/>
+			</div>
+			
+		</div>
+		<div class="divider text-white divider-accent sm:text-4xl sm:mt-8">
+		</div>
+		<!---->
+		<div class="divider text-white divider-accent sm:text-4xl sm:mt-8">
+		</div>
+		
 		<div class="w-screen h-1/2 flex text-center content-center justify-center content-center">
 
 			<div class="text-center">
 				<h1 class="text-white text-2xl lg:text-3xl xl:text-5xl xl:mt-16 lg:ml-16 lg:mr-16 mt-2 font-bold mb-8 hover:animate-pulse">Schedule</h1>
-				<div class="text-white text-sm lg:text-md text-center justify-center content-center mb-4">
-
-					<table class="table-auto border-2 border-accent border-separate">
+				
+				<div class="overflow-x-auto w-screen flex justify-center mb-20">
+					<table class="table bg-black bg-opacity-40 w-1/2">
+					  <!-- head -->
+					  <thead>
+						<tr>
+						  <th class="text-white">Time</th>
+						  <th class="text-white">Event</th>
+						</tr>
+					  </thead>
+					  <tbody>
+						<tr>
+							<td class="text-white">9:30 - 10:00 A.M.</td>
+							<td class="text-white">Doors Open!</td>
+						</tr>
+						<tr>
+							<td class="text-white">10:00 - 10:15 A.M.</td>
+							<td class="text-white">Hacking Begins!!</td>
+						</tr>
+						<tr>
+							<td class="text-white">9:30 A.M.</td>
+							<td class="text-white">Breakfast</td>
+						</tr>
+						<tr>
+							<td class="text-white">10:15 - 10:30 A.M.</td>
+							<td class="text-white">Team Mixer</td>
+						</tr>
+						<tr>
+							<td class="text-white">10:30 - 11:30 A.M.</td>
+							<td class="text-white">Workshops</td>
+						</tr>
+						<tr>
+							<td class="text-white">12:00 - 12:30 P.M.</td>
+							<td class="text-white">Lunch</td>
+						</tr>
+						<tr>
+							<td class="text-white">1:30 - 2:30 P.M.</td>
+							<td class="text-white">Panelists</td>
+						</tr>
+						<tr>
+							<td class="text-white">3:30 P.M.</td>
+							<td class="text-white">Project Deadline</td>
+						</tr>
+						<tr>
+							<td class="text-white">3:30 - 4:30 P.M.</td>
+							<td class="text-white">Demos</td>
+						</tr>
+						<tr>
+							<td class="text-white">4:30 - 4:45 P.M.</td>
+							<td class="text-white">Judging/Voting</td>
+						</tr>
+						<tr>
+							<td class="text-white">4:45 - 5:00 P.M.</td>
+							<td class="text-white">Winner and Award Ceremony</td>
+						</tr>
+					  </tbody>
+					</table>
+				  </div>
+					<!-- <table class="table-auto border-2 border-accent border-separate">
 						<tr>
 							<th class="border border-accent">Time</th>
 							<th class="border border-accent">Event</th>
@@ -139,16 +201,16 @@
 							<td>4:45 - 5:00 P.M.</td>
 							<td>Winner and Award Ceremony</td>
 						</tr>
-					</table>
+					</table> -->
 
-				</div>
+				
 
 
 			</div>
 		</div>
 
 
-`
-<div class="footer border-t-2 border-white h-10 text-center bg-black justify-center"><p class="mt-2 text-white">© Maneframe 2024</p></div>
+<br>
+
 </body>
 </html>
--- maneframe/tailwind.config.js
+++ maneframe/tailwind.config.js
@@ -3,7 +3,19 @@ export default {
   content: ["./src/**/*.{svelte,ts,js,html}", "./src/routes/**/*.{svelte,ts,js,html}"],
   theme: {
     extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
       keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
         bounce_small: {
           '0%, 100%': {
             transform: 'translateY(-5%)',

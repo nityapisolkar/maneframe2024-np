e23147099b744456932d847f237dd75d6791a495
nitya <75457173+nityapisolkar@users.noreply.github.com>
2024-05-24 22:36:34 -0700

    navbar added

--- maneframe/src/routes/+page.svelte
+++ maneframe/src/routes/+page.svelte
@@ -7,6 +7,20 @@
 
 	import Typewriter from 'svelte-typewriter';
 
+	window.onscroll = function() {myFunction()};
+
+	var navbar = document.getElementById("navbar");
+	var sticky = navbar.offsetTop;
+
+	function navbarSticky() {
+		if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky")
+  		} else {
+		navbar.classList.remove("sticky");
+		}
+	}
+
+
 </script>
 
 <html lang="en">
